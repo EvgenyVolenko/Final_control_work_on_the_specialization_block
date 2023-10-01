c2ef5669ec56f877e1eede1bbf92a563c4f52514
Evgeny <evolenko@inbox.ru>
2023-10-01 21:48:37 +0300

    Added Printer and TypeHandler

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 import model.*;
-import views.Menu;
-import views.Printer;
+import views.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,10 +7,9 @@ public class Main {
         Dog dog = new Dog("Вася", "Стой, беги", "25-06-2023");
         Kamel camel = new Kamel("Вася", "Стой, беги", "02-10-2023");
 
-        // System.out.println(dog.toString() + "\n" + camel.toString());
-
         Menu.printMenu();
 
         Printer.PrintAnimal(dog);
+        Printer.PrintAnimal(camel);
     }
 }
