0a27e8ff1500a0e788806767fd68256311a33968
Evgeny <evolenko@inbox.ru>
2023-10-01 22:29:31 +0300

    Added ListOfAnimals

--- Main.java
+++ Main.java
@@ -8,12 +8,8 @@ public class Main {
 
         listOfAnimals.addAnimal(new Dog("Вася", "Стой, беги", "25-06-2023"))
                      .addAnimal(new Kamel("Вася", "Стой, беги", "02-10-2023"));
-        
-        // Dog dog = new Dog("Вася", "Стой, беги", "25-06-2023");
-        // Kamel camel = new Kamel("Вася", "Стой, беги", "02-10-2023");
-
+      
         MenuText.printMenu();
-
         for (Animal animal : listOfAnimals) {
             Printer.PrintAnimal(animal);
         }
