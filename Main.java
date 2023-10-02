6786b12c44c945c2161cd90455d135f0e4be6863
Evgeny <evolenko@inbox.ru>
2023-10-03 01:14:03 +0300

    Correct scaner close

--- Main.java
+++ Main.java
@@ -11,7 +11,7 @@ public class Main {
 
         listOfAnimals.addAnimal(new Dog("Тузик", new ArrayList<>(Arrays.asList("Стоять", "Бежать")), "03-12-2018"))
                      .addAnimal(new Cat("Муся", new ArrayList<>(Arrays.asList("Вперед")), "03-12-2018"))
-                     .addAnimal(new Donkey("Иа", new ArrayList<>(Arrays.asList("Страдать", "Совать")), "03-12-2018"));
+                     .addAnimal(new Donkey("Иа", new ArrayList<>(Arrays.asList("Страдать", "Бухтеть")), "03-12-2018"));
                      
         MenuListOfAnimals list = new MenuListOfAnimals();
         list.run(listOfAnimals);
